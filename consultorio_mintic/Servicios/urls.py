824d6f0fd931f3405d5f8ec79eba61f1cc42718e
Alejandro Lopez Velez <Inglopezvelez@hotmail.com>
2021-09-20 22:26:37 -0500

    Borrado Desarrollo Anterior

--- consultorio_mintic/Servicios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.urls.resolvers import URLPattern
-from Servicios.views import vistaServicios
-#Servicios - Aplicacion
-#No es obligatorio pero si recomendable
-
-urlpatterns = [
-    path('servicios', vistaServicios)
-]
